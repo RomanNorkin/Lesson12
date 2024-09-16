ce3bb1bfde6e44f07bd41002e53bd3ae19224bb5
Roman Norkin <vbybrhfan@yandex.ru>
2024-09-16 17:11:34 +0300

    Lesson_12_junit_5

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,55 @@
-public class Main {
-    public static void main(String[] args) {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+public class Main {
+
+    int n = 30;
+
+    @BeforeAll
+    public static void lesson12Junit5() {
+        System.out.println("Юнит-тест для вычисления факториала числа используя Junit 5.\n");
     }
+
+    @Test
+    public void factorialNegativeNum() {
+
+        if (n < 0) {
+
+            throw new IllegalArgumentException("Для отрицательных чисел нельзя вычислить факториал");
+
+        }
+    }
+
+    @Test
+    public void factorialEqualZero() {
+        if (n == 0) {
+
+            System.out.println("Факториал числа " + n + " равен:\n");
+            System.out.println("1");
+
+        }
+    }
+
+    @Test
+    public void factorial() {
+
+        if (n <= 0) {
+            return;
+        }
+
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            result *= i;
+
+        }
+
+        System.out.println("Факториал числа " + n + " равен:\n");
+        System.out.println(result);
+    }
+
+
 }
+
+
